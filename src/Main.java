115a604d9cfb747fc96ab2c239b71923379e34f8
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 15:56:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main (String []args) {
+
+
+        int x = 3;
+        int y = 4;
+
+        System.out.println(add(x,y));
+
+        }
+
+        static int add (int x , int y){
+            return x + y;
+            
+
+        }
+
+
+
+
+
+
+    }
+
+
